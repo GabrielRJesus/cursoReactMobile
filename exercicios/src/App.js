863e5a579984091cc6292fa556a62060eadda8a6
GabrielRJesus <gabrielrj623@hotmail.com>
2019-11-10 19:16:05 -0300

    aula20

---
+++ exercicios/src/App.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react'
+import {View, Text, StyleSheet} from 'react-native'
+
+export default class App extends Component{
+  render(){
+    return(
+      <View style={style.container}>
+        <Text style={style.f20}>App!</Text>
+      </View>
+    )
+  }
+}
+
+const style = StyleSheet.create({
+  container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center'
+  },
+  f20:{
+    fontSize: 40
+  }
+})
